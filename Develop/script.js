2fbeaeafa866ffa06512e964b6b1f3d0b9c9df94
Alfonso Robles <fons3517@gmail.com>
2021-08-26 12:28:22 -0500

    added timeblocks in html and set times

---
+++ Develop/script.js
@@ -0,0 +1,14 @@
+$('')
+// I am using a daily planner to create a schedule
+
+// When I open the planner, the current day is displayed at the top of the calendar
+
+// When I scroll, I am presented with timeblocks for standard business hours
+
+// When I view timeblocks for that day, each block is color coded to indicate whether it's in past, present, or future
+
+// When I click into a timeblock, then I can enter an event
+
+// When I click Save button for timeblock, then the text for that event is saved in local storage
+
+// When I refresh page, then saved events persist
