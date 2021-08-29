33af3fcb7a80e2c0436af79422ee6b9558d8b277
Alfonso Robles <fons3517@gmail.com>
2021-08-28 21:36:16 -0500

    deleted readme text on script.js page

--- Develop/script.js
+++ Develop/script.js
@@ -28,17 +28,4 @@ for(var i = 9; i <= 17; i++) {
     else{
         $(`#userEntry-${i}`).addClass('future')
     }
-}
-// I am using a daily planner to create a schedule
-
-// When I open the planner, the current day is displayed at the top of the calendar
-
-// When I scroll, I am presented with timeblocks for standard business hours
-
-// When I view timeblocks for that day, each block is color coded to indicate whether it's in past, present, or future
-
-// When I click into a timeblock, then I can enter an event
-
-// When I click Save button for timeblock, then the text for that event is saved in local storage
-
-// When I refresh page, then saved events persist
+};
